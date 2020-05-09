5761087d4cfcc8afbbcede97017247ba1e1e9799
sriramede9 <only4programming03@gmail.com>
2020-05-09 09:17:12 +0100

    #Subject instad of EventEmitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { UserService } from "./user.service";
+import { Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  isActivated: boolean = false;
+  private isActivatedSubscribe: Subscription;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    // this.userService.activatedEmitter.subscribe((x) => (this.isActivated = x));
+
+    this.isActivatedSubscribe = this.userService.activatedSubject.subscribe(
+      (d) => (this.isActivated = d)
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.isActivatedSubscribe.unsubscribe();
+  }
 }
